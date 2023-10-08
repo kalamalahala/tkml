379683bcbb10c822de40444210f65bce91fabfc6
kalamalahala <solo.driver.bob@gmail.com>
2023-10-08 18:18:51 -0400

    code analysis cleanup

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 /**
  * The header for our theme
  *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
+ * This is the template that displays all the <head> section and everything up until <div id="content">
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
